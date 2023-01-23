93c9d55849a183b94796901f906082d611e7b82d
Sergey Armyaninov <asm-techno2@yandex.ru>
2023-01-23 20:14:37 +0500

    example 1 to 5

---
+++ Example 1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.Write("Введите число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double b = a * a;
+//Console.WriteLine($"Квадрат числа равен {b}");
+//или
+Console.WriteLine("Квадрат числа равен " + b);
---
+++ Example 3_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу,
+// которая будет выдавать название дня недели по заданному номеру
+// Вариант Switch case
+
+Console.Write("Введите число от 1 до 7: ");
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+case 1:
+    Console.Write("Понедельник");
+    break;
+case 2:
+    Console.Write("Вторник");
+    break;
+case 3:
+    Console.Write("Среда");
+    break;
+case 4:
+    Console.Write("Четверг");
+    break;
+case 5:
+    Console.Write("Пятница");
+    break;
+case 6:
+    Console.Write("Суббота");
+    break;
+case 7:
+    Console.Write("Воскресенье");
+    break;
+default:
+    Console.Write("В неделе 7 дней. Введите число от 1 до 7");
+    break;
+};
+
